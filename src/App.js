9d946861abb18bce94fcba3076697b4f12597e03
jyoti-wp <sim_s006@icloud.com>
2021-01-08 20:39:29 +0530

    landing page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/layout/header';
+import Footer from './components/layout/footer';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Header />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <Footer />
       </header>
     </div>
   );
