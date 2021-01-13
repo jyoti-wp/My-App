e920ee1ac5b194aa4356e21d0e8a78a2d2f4bd00
jyoti-wp <sim_s006@icloud.com>
2021-01-13 19:24:30 +0530

    background image added

--- src/components/layout/Page/index.js
+++ src/components/layout/Page/index.js
@@ -1,12 +1,12 @@
 function Page () {
     return (
-        <div class="page-banner">
-            <div class="page-banner-image"></div>
-            <div class="page-banner-content container">
-                <h1 class="headline-large">Welcome!</h1>
-                <h2 class="headline-medium">We think you&rsquo;ll like it here.</h2>
-                <h3 class="headline-small">Where can I get some? There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</h3>
-                <a href="#" class="click-btn">Find Your Major</a>
+        <div className="page-banner">
+            <div className="page-banner-image"></div>
+            <div className="page-banner-content container">
+                <h1 className="headline-large">Welcome!</h1>
+                <h2 className="headline-medium">We think you&rsquo;ll like it here.</h2>
+                <h3 className="headline-small">Where can I get some? There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</h3>
+                <a href="#" className="click-btn">Find Your Major</a>
             </div>
         </div>
     )
